209b317e990b62bae786fa5444f2c19d19e26a0e
keniiy <kehindekehinde894@gmail.com>
2020-05-31 23:51:22 -0800

    fully functional

--- user/views.py
+++ user/views.py
@@ -1,13 +1,32 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
 # Create your views here.
 from product.models import Category
+from user.models import UserProfile
 
 
 def index(request):
     return HttpResponse("User App")
 
 def login_form(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            current_user = request.user
+            userprofile = UserProfile.objects.get(user_id=current_user.id)
+            request.session['userimage'] = userprofile.image.url
+            # Redirect to a success page.
+            return HttpResponseRedirect('/')
+        else:
+            # Return an 'invalid login' error message.
+            messages.warning(request, "Login Error, Incorrect Username Or Password!!")
+            return HttpResponseRedirect('/login')
     category = Category.objects.all()
     context = {
         'category': category
