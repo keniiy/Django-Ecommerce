6068579b9cd5568cee41b0e894498b75a86ca240
keniiy <kehindekehinde894@gmail.com>
2020-06-07 03:56:50 -0800

    auto image update on sign up/ and partially done order configuration

--- user/views.py
+++ user/views.py
@@ -45,6 +45,13 @@ def signup_form(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
+            #create data in profile table for user
+            current_user = request.user
+            data = UserProfile()
+            data.user_id = current_user.id
+            data.image = "images/users/user.png"
+            data.save()
+            messages.success(request, 'Your account has been created!')
             return HttpResponseRedirect('/')
         else:
             messages.warning(request, form.errors)
